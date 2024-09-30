1dc143ac45ebc6b356ef8f3b68820c59a93e0114
Simão Pedro <simaopgt@MacBooks-MacBook-Pro.local>
2024-09-30 01:17:04 -0300

    Removes preview from MainActivity

--- app/src/main/java/com/idk/androidswissarmyknife/MainActivity.kt
+++ app/src/main/java/com/idk/androidswissarmyknife/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import com.idk.androidswissarmyknife.ui.theme.AndroidSwissArmyKnifeTheme
 import com.idk.feature_home.presentation.ui.HomeScreen
 
@@ -18,13 +16,5 @@ class MainActivity : ComponentActivity() {
                 HomeScreen()
             }
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidSwissArmyKnifeTheme {
-        HomeScreen()
     }
 }
